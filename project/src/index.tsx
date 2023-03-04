f266c410dcfe693698bbc65462a7c48a026ee073
MahaLego <legostina@photolab.me>
2023-03-04 17:22:06 +0700

    Пишет реакт компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PlaceSelection: 320,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placeSelection = {Setting.PlaceSelection}
+    />
   </React.StrictMode>,
 );
