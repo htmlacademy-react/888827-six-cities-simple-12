23a97450f080fb9b4f609856eb2b1d2a49b64bb0
MahaLego <legostina@photolab.me>
2023-04-14 17:41:20 +0700

    Добавляет memo для сортировки

--- project/src/components/sort-options/sort-options.tsx
+++ project/src/components/sort-options/sort-options.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, memo} from 'react';
 import {changeOption} from '../../store/offer-data/offer-data';
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {OPTIONS} from '../const/const';
@@ -40,4 +40,4 @@ function SortOptions():JSX.Element {
   );
 }
 
-export default SortOptions;
+export default memo(SortOptions);
--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,6 +1,4 @@
 import {createAction} from '@reduxjs/toolkit';
 import {AppRoute} from '../components/const/const';
 
-//export const resetOffers = createAction<{resetedOffers: number}>('offer/resetOffers');
-
 export const redirectToRoute = createAction<AppRoute>('offer/redirectToRoute');
